214f7c97306157578dff90ae66c3dd4503f4415d
daniellee0911 <daniellee860911@gmail.com>
2024-09-25 11:02:55 +0800

    feat: 改用 MongoDB 紀錄 request log

--- app/Http/Middleware/LogRequest.php
+++ app/Http/Middleware/LogRequest.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class LogRequest
 {
@@ -18,20 +19,17 @@ class LogRequest
     public function handle(Request $request, Closure $next): Response
     {
         if(config('app.env')!=='testing'){
-            Log::build([
-                'driver' => 'daily',
-                'path' => storage_path('logs/request.log'),
-                'days' => env('LOG_DAILY_DAYS', 14),
-                'replace_placeholders' => true,
-              ])->info(json_encode([
-                'app_env' => config('app.env'),
-                'datetime' => Carbon::now()->toIso8601String(),
-                'user_id' => auth()->user() ? auth()->user()->id : 0,
-                'method' => request()->getMethod(),
-                'ip' => request()->ip(),
-                'uri' => request()->getPathInfo(),
-                'user_agent' => request()->userAgent(),
-            ]));
+            DB::connection('mongodb')
+                ->table('request/'.date("Y-m-d"))
+                ->insert([
+                    'app_env' => config('app.env'),
+                    'datetime' => Carbon::now()->toIso8601String(),
+                    'user_id' => auth()->user() ? auth()->user()->id : 0,
+                    'method' => request()->getMethod(),
+                    'ip' => request()->ip(),
+                    'uri' => request()->getPathInfo(),
+                    'user_agent' => request()->userAgent(),
+            ]);
         }
         return $next($request);
     }
